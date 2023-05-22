908c3e4e10e89c8e1a99b7117e66ad97b370ac40
dmitrykovalevsky89 <dmitryk@gophotoweb.ru>
2023-05-22 16:38:28 +0300

    Lesson 03

--- firstApp/App.js
+++ firstApp/App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { StatusBar } from "expo-status-bar"
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Button,
+  Alert,
+  Image,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+} from "react-native"
 
 export default function App() {
+  const [text, setText] = React.useState("Hello!")
+
+  const handlePress = () => {
+    setText("Hello, World!")
+  }
+
+  const handleButtonPress = () => {
+    Alert.alert("Title", "Text", [
+      { text: "Ok", onPress: () => console.log("Ok") },
+      { text: "Cancel", onPress: () => console.log("Cancel") },
+    ])
+  }
+
+  const handleButtonPressPromt = () => {
+    Alert.prompt("Title", "Text", (text) => console.log(text))
+  }
+
+  const handleImagePress = () => {
+    console.log("Image")
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} style={styles.text} onPress={handlePress}>
+        {text}
+      </Text>
+      <Button title="Open alert" onPress={handleButtonPress} />
+      <Button title="Open promt" onPress={handleButtonPressPromt} />
+      <TouchableWithoutFeedback onPress={handleImagePress}>
+        <Image style={styles.image} source={require("./assets/adaptive-icon.png")} />
+      </TouchableWithoutFeedback>
+
       <StatusBar style="auto" />
-    </View>
-  );
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f4f4f2",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+  text: {
+    fontSize: "16px",
+    color: "#261130",
+    marginBottom: 10,
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+})
